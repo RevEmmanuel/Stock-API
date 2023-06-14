e3c4c9c4452e7de633eaee72b5eae56a111c47ce
RevEmmanuel <adeolaae1@gmail.com>
2023-06-14 12:11:16 +0100

    configurations

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,8 +11,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @OpenAPIDefinition(
         info = @Info(
                 title = "Stock API",
-                description = "This application handles basic services for Stock Entities."
-        ))
+                description = "This application handles basic services for Stock Entities.",
+                version = "v1",
+                contact = @Contact(
+                        name = "Adeola Adekunle",
+                        email = "adeolaae1@gmail.com"
+                        )
+        ),
+        externalDocs = @ExternalDocumentation(
+                url= "https://",
+                description = "Postman Documentation"
+        )
+)
 public class Main {
 
     public static void main(String[] args) {
